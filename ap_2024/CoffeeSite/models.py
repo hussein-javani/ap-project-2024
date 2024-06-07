2aee46bf638521846753bbed9ba001516d942c5d
NedaAbedini <nedaabed03@gmail.com>
2024-06-07 15:29:07 +0330

    defining methods to handle orders and storage , handling a bug.

--- ap_2024/CoffeeSite/models.py
+++ ap_2024/CoffeeSite/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.contrib import admin
 from django.core.exceptions import ValidationError
 
-class storage(models.Model):
+class Storage(models.Model):
     id = models.AutoField(primary_key= True)
     name = models.CharField(max_length=255 , unique= True , null= False , blank= False)
     amount = models.PositiveIntegerField(null= False , blank= False , validators= [
@@ -22,13 +22,13 @@ class Products(models.Model) :
     coffee = models.PositiveIntegerField(null = False , blank=False)  
     flour= models.PositiveIntegerField(null= False , blank= False)
     chocolate = models.PositiveIntegerField(null= False , blank= False)
-    vertical = models.BinaryField(max_length=10) #ASk
+    vertical = models.CharField(max_length=255 , null= False , blank= False)
 
     def check_storage(self , quantity) : 
-        stored_suger = storage.objects.filter(name = "suger").first()
-        stored_coffee = storage.objects.filter(name = "coffee").first()
-        stored_flour = storage.objects.filter(name = "flour").first()
-        stored_chocolate = storage.objects.filter(name = "chocolate").first()
+        stored_suger = Storage.objects.filter(name = "suger").first()
+        stored_coffee = Storage.objects.filter(name = "coffee").first()
+        stored_flour = Storage.objects.filter(name = "flour").first()
+        stored_chocolate = Storage.objects.filter(name = "chocolate").first()
 
     
         return (stored_suger.amount >= self.suger * quantity  and 
@@ -37,39 +37,40 @@ class Products(models.Model) :
                 stored_flour.amount >= self.flour * quantity
                 )
     
-    def update_storage(self , quantity) :
-        if self.check_storage(quantity) :
-            stored_suger = storage.objects.filter(name = "suger").first().amount 
-            stored_coffee = storage.objects.filter(name = "coffee").first().amount
-            stored_flour = storage.objects.filter(name = "flour").first().amount
-            stored_chocolate = storage.objects.filter(name = "chocolate").first().amount
+    def update_Storage(self , quantity) :
+        if self.check_Storage(quantity) :
+            storage_sugar = Storage.objects.get(name="sugar")
+            storage_coffee = Storage.objects.get(name="coffee")
+            storage_flour = Storage.objects.get(name="flour")
+            storage_chocolate = Storage.objects.get(name="chocolate")
 
-            #reducing 
-            stored_suger -= self.suger * quantity
-            stored_flour -= self.flour * quantity
-            stored_coffee -= self.coffee * quantity
-            stored_chocolate -= self.chocolate * quantity
-            #saving
+            storage_sugar.amount -= self.suger * quantity
+            storage_coffee.amount -= self.coffee * quantity
+            storage_flour.amount -= self.flour * quantity
+            storage_chocolate.amount -= self.chocolate * quantity
 
-            stored_suger.save()
-            stored_flour.save()
-            stored_coffee.save()
-            stored_chocolate.save()
+            storage_sugar.save()
+            storage_coffee.save()
+            storage_flour.save()
+            storage_chocolate.save()
 
-        
-        
-
-
-class Orders(models.Model) :  #سبد خرید
+class Orders(models.Model) :
     order_id = models.AutoField(primary_key=True , unique= True)
     username = models.CharField(max_length=255)
     product = models.CharField(max_length=255)
     purchase_amount = models.IntegerField()
-    type = models.BooleanField(default= True) # is 1 if the order is take out and 0 if
+    type = models.BooleanField(default= True) # is 1 if the order is take out and 0 if not. 
 
     
     def confirm_order(self) :
-        pass
+        product = self.product
+        quantity = self.purchase_amount
+        if Products.objects.get(name = product).check_storage(quantity) :
+            self.save()
+            Products.objects.get(name = product).update_storage(quantity)
+        else : 
+            raise ValidationError("Sorry, we can't take your order.")
+        
 
 class Orders_Product(models.Model) : 
     id = models.AutoField(primary_key= True)
