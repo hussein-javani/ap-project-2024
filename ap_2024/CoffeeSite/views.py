25e985988fde623399bb32dfc89af6e455cd655e
seeppi <sepehr.sohrabi2003@gmail.com>
2024-06-25 00:40:34 +0330

    Update views.py

--- ap_2024/CoffeeSite/views.py
+++ ap_2024/CoffeeSite/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
+from django.views import View
+from .forms import SignUpForm, LoginForm
 
-# Create your views here.
+class LoginView(View):
+    def get(self, request):
+        form = LoginForm()
+        return render(request, 'login.html', {'form': form})
+
+    def post(self, request):
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  
+        return render(request, 'login.html', {'form': form})
+
+class SignUpView(View):
+    def get(self, request):
+        form = SignUpForm()
+        return render(request, 'signup.html', {'form': form})
+
+    def post(self, request):
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password1'])
+            user.save()
+            login(request, user)
+            return redirect('home')  
+        return render(request, 'signup.html', {'form': form})
